58a73fef0e045cd2abeddd6eb7c11855701c31ea
Puru Vijay <awesomepuruvj@gmail.com>
2022-02-04 15:44:27 +0530

    Commit

--- sdk/src/generate.ts
+++ sdk/src/generate.ts
@@ -7,11 +7,12 @@ const SUPPORTED_TEMPLATES: Project['template'][] = [
   'javascript',
   'node',
   'polymer',
+  'html',
   'typescript',
   'vue',
 ];
 
-function createHiddenInput(name: string, value: string){
+function createHiddenInput(name: string, value: string) {
   const input = document.createElement('input');
   input.type = 'hidden';
   input.name = name;
@@ -19,8 +20,8 @@ function createHiddenInput(name: string, value: string){
   return input;
 }
 
-function createProjectForm(project: Project){
-  if(SUPPORTED_TEMPLATES.indexOf(project.template) === -1){
+function createProjectForm(project: Project) {
+  if (SUPPORTED_TEMPLATES.indexOf(project.template) === -1) {
     console.warn(`Unsupported project template, must be one of: ${SUPPORTED_TEMPLATES.join(', ')}`);
   }
 
@@ -33,34 +34,34 @@ function createProjectForm(project: Project){
   form.appendChild(createHiddenInput('project[description]', project.description));
   form.appendChild(createHiddenInput('project[template]', project.template));
 
-  if(project.dependencies){
-    form.appendChild(createHiddenInput('project[dependencies]', JSON.stringify(project.dependencies)));
+  if (project.dependencies) {
+    form.appendChild(
+      createHiddenInput('project[dependencies]', JSON.stringify(project.dependencies))
+    );
   }
 
-  if(project.settings){
+  if (project.settings) {
     form.appendChild(createHiddenInput('project[settings]', JSON.stringify(project.settings)));
   }
 
-  Object.keys(project.files).forEach(path => {
+  Object.keys(project.files).forEach((path) => {
     form.appendChild(createHiddenInput(`project[files][${path}]`, project.files[path]));
   });
 
   return form;
 }
 
-export function createProjectFrameHTML(project: Project, options?: EmbedOptions){
+export function createProjectFrameHTML(project: Project, options?: EmbedOptions) {
   const form = createProjectForm(project);
   form.action = `${getOrigin(options)}/run` + buildProjectQuery(options);
   form.id = 'sb';
 
-  const html = `<html><head><title></title></head><body>${
-    form.outerHTML
-  }<script>document.getElementById('sb').submit();</script></body></html>`;
+  const html = `<html><head><title></title></head><body>${form.outerHTML}<script>document.getElementById('sb').submit();</script></body></html>`;
 
   return html;
 }
 
-export function openProject(project: Project, options?: OpenOptions){
+export function openProject(project: Project, options?: OpenOptions) {
   const form = createProjectForm(project);
   form.action = `${getOrigin(options)}/run` + buildProjectQuery(options);
   form.target = openTarget(options);
