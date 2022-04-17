93d74ae499eb90a9a208b2f6fb1b5efcc8262ee6
Florens Verschelde <florens@fvsch.com>
2022-04-17 13:33:27 +0200

    sdk: list exported types and methods explicitly

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,3 +1,15 @@
+import {
+  connect,
+  embedGithubProject,
+  embedProject,
+  embedProjectId,
+  openGithubProject,
+  openProject,
+  openProjectId,
+} from './lib';
+
+// Explicitly export public types (vs using `export * from './interfaces'`),
+// so that additions to interfaces don't become a part of our public API by mistake.
 export type {
   Project,
   ProjectOptions,
@@ -13,5 +25,16 @@ export type {
 } from './interfaces';
 export type { FsDiff, VM } from './vm';
 
-import * as StackBlitzSDK from './lib';
-export default StackBlitzSDK;
+// Export a single object with methods, for compatibility with UMD and CommonJS.
+// Ideally we would also have named exports, but that can create incompatibilities
+// with some bundlers, and microbundle doesn't support it:
+// https://github.com/developit/microbundle/issues/712
+export default {
+  connect,
+  embedGithubProject,
+  embedProject,
+  embedProjectId,
+  openGithubProject,
+  openProject,
+  openProjectId,
+};
