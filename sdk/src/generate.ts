a18dfbaa9c9dcb1e20055a68bde6c28ad714a0b0
Florens Verschelde <florens@fvsch.com>
2022-04-18 22:25:47 +0200

    sdk: export more types, add showSidebar option and vm.editor.setCurrentFile (#1837)
    * fix vm.editor.getUrl returning an object and not a string
    * improve types of the RDC class
    * export all types from src/interfaces.ts
    * refactor URL param generation
    * add showSidebar option
    * sdk: add experimental vm.editor.setCurrentFile
    * bump version to 1.7.0-alpha.3

--- sdk/src/generate.ts
+++ sdk/src/generate.ts
@@ -1,16 +1,6 @@
-import { Project, EmbedOptions, OpenOptions } from './interfaces';
+import type { Project, EmbedOptions, OpenOptions } from './interfaces';
+import { PROJECT_TEMPLATES } from './constants';
 import { embedUrl, openTarget, openUrl } from './helpers';
-
-const SUPPORTED_TEMPLATES: Project['template'][] = [
-  'angular-cli',
-  'create-react-app',
-  'html',
-  'javascript',
-  'node',
-  'polymer',
-  'typescript',
-  'vue',
-];
 
 function createHiddenInput(name: string, value: string) {
   const input = document.createElement('input');
@@ -21,8 +11,9 @@ function createHiddenInput(name: string, value: string) {
 }
 
 function createProjectForm(project: Project) {
-  if (!SUPPORTED_TEMPLATES.includes(project.template)) {
-    console.warn(`Unsupported project.template: must be one of ${SUPPORTED_TEMPLATES.join(', ')}`);
+  if (!PROJECT_TEMPLATES.includes(project.template)) {
+    const names = PROJECT_TEMPLATES.map((t) => `'${t}'`).join(', ');
+    console.warn(`Unsupported project.template: must be one of ${names}`);
   }
 
   const isWebContainers = project.template === 'node';
--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,5 +1,40 @@
-export type { Project, OpenOptions, EmbedOptions } from './interfaces';
-export type { VM } from './VM';
+import {
+  connect,
+  embedGithubProject,
+  embedProject,
+  embedProjectId,
+  openGithubProject,
+  openProject,
+  openProjectId,
+} from './lib';
 
-import * as StackBlitzSDK from './lib';
-export default StackBlitzSDK;
+// Explicitly export public types (vs using `export * from './interfaces'`),
+// so that additions to interfaces don't become a part of our public API by mistake.
+export type {
+  Project,
+  ProjectOptions,
+  ProjectDependencies,
+  ProjectFiles,
+  ProjectSettings,
+  ProjectTemplate,
+  EmbedOptions,
+  OpenOptions,
+  OpenFileOption,
+  UiThemeOption,
+  UiViewOption,
+} from './interfaces';
+export type { FsDiff, VM } from './vm';
+
+// Export a single object with methods, for compatibility with UMD and CommonJS.
+// Ideally we would also have named exports, but that can create incompatibilities
+// with some bundlers, and microbundle doesn't support it:
+// https://github.com/developit/microbundle/issues/712
+export default {
+  connect,
+  embedGithubProject,
+  embedProject,
+  embedProjectId,
+  openGithubProject,
+  openProject,
+  openProjectId,
+};
